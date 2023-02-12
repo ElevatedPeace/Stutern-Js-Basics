3b344326557f97f541e1501635f32db6e0aa1cd7
ElevatedPeace <99437895+ElevatedPeace@users.noreply.github.com>
2023-02-12 15:52:16 +0100

    javascript basics

---
+++ index.js
@@ -0,0 +1,23 @@
+// Creating a function that multiplies two numbers of my choice.
+
+function multiply(x, y) {
+return x * y
+}
+console.log(multiply(3,4));
+
+// Creating variables.
+
+let firstName= "Peace";
+let lastName= "Eleke";
+let track= "Frontend Engineering";
+let skills="researching";
+let favoriteColor= "Purple";
+let IndependenceYear= "1960";
+
+// Making a sentence with these variables through concatenating.
+
+let sentence= "My name is " + (firstName) + " "+ (lastName) + ". I am studying " +
+(track)+ " at Stutern. I love "+ (skills)+ " and my favorite color is "+ (favoriteColor)
++ ". My country had their independence in " + (IndependenceYear) + "."
+
+console.log(sentence);
